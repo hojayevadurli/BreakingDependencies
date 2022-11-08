3127cccf2fa8faf176d3ce3227aba44106bdc5cc
durli.hojayeva <hojayevadurli@gmail.com>
2022-11-07 21:59:55 -0700

    added interface and class #2

--- WinFormsApp1/Form1.cs
+++ WinFormsApp1/Form1.cs
@@ -1,10 +1,45 @@
+using System.Runtime.CompilerServices;
+
 namespace WinFormsApp1
 {
+
+    public interface IForm
+    {
+        public void DisplayMessage(string message);
+
+    }
+
+    public partial class Form2 : IForm
+    {
+        public void DisplayMessage(string message)
+        {
+            MessageBox.Show(message);
+        }
+    }
+
+
     public partial class Form1 : Form
     {
+
+        IForm dependency;
+
+        public string errorMessage { get; set; }
         public Form1()
         {
             InitializeComponent();
+        }
+
+        public Form1(IForm form, string message)
+        {
+            InitializeComponent();
+            //this.errorMessage = message;
+            this.dependency = form;
+
+            form.DisplayMessage(message);
+            //dependency.DisplayMessage(message);
+
+
+
         }
 
         public void btnCalculate_Click(object sender, EventArgs e)
