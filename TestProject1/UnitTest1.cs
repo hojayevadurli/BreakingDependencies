d3862e9943e5b3bb0e53d0fdde4d92f5967fc0ca
durli.hojayeva <hojayevadurli@gmail.com>
2022-11-07 22:01:52 -0700

    added a new class to test project

--- TestProject1/UnitTest1.cs
+++ TestProject1/UnitTest1.cs
@@ -1,8 +1,19 @@
+using Microsoft.VisualStudio.TestPlatform.CommunicationUtilities;
 using Moq;
 using WinFormsApp1;
 
 namespace TestProject1
 {
+
+    public class TestFormDependency : IForm
+    {
+        public string Message { get; set; }
+
+        public void DisplayMessage(string message)
+        {
+            Message = message;
+        }
+    }
     public class Tests
     {
         [SetUp]
@@ -11,17 +22,27 @@ namespace TestProject1
 
         }
 
+
+
         [Test]
 
         public void CountButtonClick()
         {
-            Assert.That(() => form.Message == "You have to enter a name");
+            var formDependency = new TestFormDependency();
+            var form1 = new Form1();
+            form1.btnCalculate_Click(null, null);
+            Assert.That(() => formDependency.Message == "You have to enter a name");
+
         }
 
-        [Test]
-        public void NumBalanceError()
-        {
-            Assert.That(() => form.Message == "Must be between 10k and 1MM");
-        }
+        //[Test]
+        //public void NumBalanceError()
+        //{
+        //    var form1 = new Form1();
+        //    var form2=new Form2();
+        //    form1.btnCalculate_Click(null, null);
+        //    Assert.That(() => form1.Message == "Must be between 10k and 1MM");
+        //}
     }
 }
+
